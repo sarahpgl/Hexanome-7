4cb5f6eca3f2cd93c3270026f904e863b0a8b3c3
ThibautChantrel <thibaut.chantrel0102@gmail.com>
2023-11-07 18:33:52 +0100

    Ajout de certaines modifications du catalogueTour

--- Delifery/src/Donnees/CatalogueTours.java
+++ Delifery/src/Donnees/CatalogueTours.java
@@ -1,15 +1,32 @@
 package Donnees;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatalogueTours {
 
     public List<Tour> catalogue;
 
+    public CatalogueTours(){
+        catalogue= new ArrayList<>();
+    }
+
     public Tour getTourById(Long id) {
         for(Tour tour: catalogue) {
             if(tour.getId() == id) return tour;
         }
         return null;
+    }
+
+    public void ajouterTour(Tour tour){
+        catalogue.add(tour);
+    }
+
+    public String toString(){
+        String mes = "Catalogue Tour : \n";
+        for (Tour t: catalogue) {
+            mes = mes + t.toString() +"\n";
+        }
+        return mes;
     }
 }
