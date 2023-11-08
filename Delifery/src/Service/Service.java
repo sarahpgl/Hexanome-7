9a61ba0efb6c48e0d1cf30a7d6ea7083e24ecfcf
ThibautChantrel <thibaut.chantrel0102@gmail.com>
2023-11-08 09:09:36 +0100

    Ajout de la méthode getAllIntersectionss()

--- Delifery/src/Service/Service.java
+++ Delifery/src/Service/Service.java
@@ -248,4 +248,16 @@ public class Service {
     public void sauvegarderCatalogueTourXML (CatalogueTours Ctour, String chemin, String nomFichier){
         FileSystemXML.EcrireCatalogueXML(Ctour,chemin,nomFichier);
     }
+
+    public List<Intersection> getAllIntersections() {
+        // On crée une liste vide pour stocker les intersections
+        List<Intersection> intersections = new ArrayList<>();
+        // On parcourt l'ensemble des clés de la carte
+        for (Intersection key : donneesCarte.getCarte().keySet()) {
+            // On ajoute chaque clé à la liste
+            intersections.add(key);
+        }
+        // On renvoie la liste
+        return intersections;
+    }
 }
