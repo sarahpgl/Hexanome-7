aef0d6a968635735f66a682ced17d31d248343c3
JadeLRX <jade.lrx0@gmail.com>
2023-11-13 11:27:43 +0100

    Javadoc dans service

--- Delifery/src/Service/Service.java
+++ Delifery/src/Service/Service.java
@@ -41,6 +41,13 @@ public class Service {
         this.catalogueTours = new CatalogueTours();
     }
 
+    /**
+     * Renvoie une instance unique de Service en utilisant le modèle Singleton.
+     * Si aucune instance n'existe, elle en crée une. Cette méthode garantit
+     * qu'une seule instance de Service est utilisée dans l'application.
+     *
+     * @return L'instance unique de Service.
+     */
     public static Service getInstance() {
         if (instance == null) {
             synchronized (Service.class) {
@@ -52,6 +59,14 @@ public class Service {
         return instance;
     }
 
+    /**
+     * Crée et initialise les données de carte à partir d'un fichier XML.
+     * Charge les informations de carte à partir du fichier spécifié, crée une carte avec
+     * des intersections, des sections, un entrepôt de départ et des informations d'échelle.
+     *
+     * @param nomFichier Le nom du fichier XML contenant les données de la carte.
+     * @return Les données de la carte créées à partir du fichier XML.
+     */
     public DonneesCarte creerDonneesCarte(String nomFichier) {
         FileSystemXML fsxml;
         fsxml = new FileSystemXML();
@@ -123,6 +138,16 @@ public class Service {
 
         return tour;
     }
+
+    /**
+     * Crée une livraison à partir des caractéristiques entrées par l'utilisateur
+     * Tente d'ajouter une livraison à un tour existant pour un livreur donné
+     *
+     * @param adresse L'intersection à laquelle la livraison est associée.
+     * @param creneau Le créneau de temps de la livraison.
+     * @param livreur Le livreur choisi par l'utilisateur.
+     * @return true si l'ajout de la livraison est réussi, false sinon.
+     */
     public Boolean essaieAjoutLivraisonAuTour(Intersection adresse, Creneau creneau, Livreur livreur){
         Livraison l = new Livraison((long)(Math.random()*1000),adresse,  creneau);
         // chercher le tour auquel correspond le livreur
@@ -147,14 +172,40 @@ public class Service {
         updateCarte();
         return true;
     }
+
+    /**
+     * Crée une instance de Livraison avec les détails spécifiés.
+     *
+     * @param id L'identifiant de la livraison.
+     * @param adresse L'intersection associée à la livraison.
+     * @param creneau Le créneau de temps de la livraison.
+     * @param heureDebut L'heure de début de la livraison.
+     * @param heureFin L'heure de fin de la livraison.
+     * @return Une nouvelle instance de Livraison avec les détails donnés.
+     */
     public Livraison creerLivraison(Long id, Intersection adresse, Creneau creneau, LocalTime heureDebut, LocalTime heureFin){
         Livraison l = new Livraison(id,adresse,  creneau,  heureDebut,  heureFin);
         return l;
     }
+
+    /**
+     * Crée une instance d'Intersection avec l'identifiant et les coordonnées spécifiés.
+     *
+     * @param id L'identifiant de l'intersection.
+     * @param coordonnees Les coordonnées de l'intersection.
+     * @return Une nouvelle instance d'Intersection avec l'identifiant et les coordonnées donnés.
+     */
     public Intersection creerIntersection(BigInteger id, Coordonnees coordonnees){
         Intersection i = new Intersection(id, coordonnees);
         return i ;
     }
+
+    /**
+     * Crée une liste d'intersections pour des simulations de livraison.
+     * Utilisé à des fins de test.
+     *
+     * @return Une liste d'intersections préparées pour des simulations de livraison.
+     */
     public ArrayList<Intersection> creerIntersectionsPourLivrer(){
         Intersection destination3 = new Intersection(new BigInteger("25321456"), new Coordonnees(45.749214,4.875591));
         Intersection destination4 = new Intersection(new BigInteger("25321433"), new Coordonnees(45.74969,4.873468));
@@ -168,6 +219,15 @@ public class Service {
         return list;
     }
 
+    /**
+     * Crée une liste de livraisons pour des intersections spécifiées, avec un identifiant et un créneau donnés.
+     * Utilisée à des fins de test.
+     *
+     * @param listeIntersections La liste des intersections pour les livraisons.
+     * @param id L'identifiant à attribuer à chaque livraison.
+     * @param c Le créneau de temps pour toutes les livraisons.
+     * @return Une liste de livraisons associées aux intersections fournies, avec l'identifiant et le créneau donnés.
+     */
     public ArrayList<Livraison> creerListeLivraisons(ArrayList<Intersection> listeIntersections, long id, Creneau c){
         ArrayList<Livraison> listeLivraison= new ArrayList<Livraison>();
         for (Intersection i : listeIntersections){
@@ -176,6 +236,16 @@ public class Service {
         }
         return listeLivraison;
     }
+
+    /**
+     * Calcule un itinéraire de livraison pour un tour donné, en respectant les créneaux de livraison.
+     *
+     * @param tour Le tour pour lequel l'itinéraire de livraison est calculé.
+     * @param vitesse La vitesse de déplacement pour le calcul de l'itinéraire.
+     * @param carte Les données de la carte utilisées pour le calcul de l'itinéraire.
+     * @param entrepot L'intersection représentant l'entrepôt de départ du tour.
+     * @return Le tour avec un itinéraire de livraison calculé respectant les créneaux.
+     */
     public Tour calculerTour (Tour tour, Double vitesse, DonneesCarte carte, Intersection entrepot){
         tour.reinitialiserTrajet();
         final int creneau8=8;
@@ -281,6 +351,12 @@ public class Service {
         return tour;
     }
 
+    /**
+     * Ouvre les détails d'un tour spécifique en créant une fenêtre dédiée pour afficher les informations.
+     *
+     * @param chemin Le chemin spécifique lié aux détails à afficher.
+     * @param id L'identifiant du tour pour lequel les détails sont ouverts.
+     */
     public void ouvrirDetails(String chemin, Long id){
         Tour t = catalogueTours.getTourById(id);
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
@@ -295,27 +371,34 @@ public class Service {
         }
     }
 
-    public DonneesCarte getDonneesCarte() {
-        return donneesCarte;
-    }
-
-    public CatalogueTours getCatalogueTours() {
-        return catalogueTours;
-    }
-
-    public void setCatalogueTours(CatalogueTours catalogueTours) {
-        this.catalogueTours = catalogueTours;
-    }
-
+    /**
+     * Sauvegarde un catalogue de tours en format XML à l'emplacement spécifié.
+     *
+     * @param Ctour Le catalogue de tours à sauvegarder.
+     * @param chemin Le chemin où le fichier XML sera sauvegardé.
+     * @param nomFichier Le nom du fichier XML.
+     */
     public void sauvegarderCatalogueTourXML (CatalogueTours Ctour, String chemin, String nomFichier){
         FileSystemXML.EcrireCatalogueXML(Ctour,chemin,nomFichier);
 
     }
+
+    /**
+     * Initialise un livreur avec un nom spécifique.
+     *
+     * @param name Le nom du livreur à initialiser.
+     * @return Une instance de livreur initialisée avec le nom spécifié.
+     */
     public Livreur initialisationLivreur(String name){
         Livreur l = new Livreur(name);
         return l;
     }
 
+    /**
+     * Récupère toutes les intersections de la carte sous forme de liste.
+     *
+     * @return Une liste contenant toutes les intersections extraites de la carte.
+     */
     public ArrayList<Intersection> getAllIntersections() {
         // On crée une liste vide pour stocker les intersections
         ArrayList<Intersection> intersections = new ArrayList<>();
@@ -328,6 +411,11 @@ public class Service {
         return intersections;
     }
 
+    /**
+     * Récupère une liste de noms des livreurs associés aux tours dans le catalogue.
+     *
+     * @return Une liste contenant les noms des livreurs présents dans le catalogue de tours.
+     */
     public ArrayList<String> getListLivreur(){
         ArrayList<String> livreurs = new ArrayList<>();
         for (Tour t: catalogueTours.catalogue) {
@@ -336,23 +424,38 @@ public class Service {
         return livreurs;
     }
 
+    /**
+     * Récupère une liste de tours du le catalogue courant.
+     *
+     * @return Une liste contenant les tours présents dans le catalogue de tours.
+     */
     public ArrayList<Tour> getTours(){
         return catalogueTours.getCatalogue();
     }
 
-    public void setVueApplication(VueApplication vueApplication) {
-        this.vueApplication = vueApplication;
-    }
 
-    public void setCarte(Carte carte){this.carte=carte;}
-    public Carte getCarte(){return this.carte;}
+    /**
+     * Met à jour la carte dans l'interface graphique de l'application.
+     * Appelle la méthode d'actualisation de la carte dans l'interface graphique.
+     */
     public void updateCarte(){
         vueApplication.updateCarte();
     }
+
+    /**
+     * Met à jour le panneau dans l'interface graphique de l'application.
+     * Appelle la méthode d'actualisation du panneau dans l'interface graphique.
+     */
     public void updatePanel(){
         vueApplication.updatePanel();
     }
 
+    /**
+     * Modifie le nombre de livreurs disponibles dans le catalogue de tours.
+     * Ajoute de nouveaux livreurs ou en supprime en fonction du nombre spécifié.
+     *
+     * @param nombre Le nombre de livreurs désiré dans le catalogue de tours.
+     */
     public void setNbLivreur(int nombre){
 
         if(nombre > catalogueTours.getListeLivreurs().size()){
@@ -367,6 +470,12 @@ public class Service {
         }
     }
 
+    /**
+     * Restitue un catalogue de tours à partir d'un fichier XML spécifié.
+     *
+     * @param cheminfihcierCatalogueTour Le chemin du fichier XML du catalogue de tours à restituer.
+     * @return true si la restitution du catalogue est réussie, false sinon.
+     */
     public boolean restituerTour(String cheminfihcierCatalogueTour){
         boolean b = false;
         FileSystemXML fsxml;
@@ -386,6 +495,53 @@ public class Service {
         return b;
     }
 
+    /**
+     * Renvoie les données de la carte.
+     *
+     * @return Les données de la carte.
+     */
+    public DonneesCarte getDonneesCarte() {
+        return donneesCarte;
+    }
 
+    /**
+     * Renvoie le catalogue de tours.
+     *
+     * @return Le catalogue de tours.
+     */
+    public CatalogueTours getCatalogueTours() {
+        return catalogueTours;
+    }
 
+    /**
+     * Définit le catalogue de tours avec celui spécifié.
+     *
+     * @param catalogueTours Le catalogue de tours à définir.
+     */
+    public void setCatalogueTours(CatalogueTours catalogueTours) {
+        this.catalogueTours = catalogueTours;
+    }
+
+    /**
+     * Définit l'instance de l'interface graphique de l'application.
+     *
+     * @param vueApplication L'instance de l'interface graphique de l'application à définir.
+     */
+    public void setVueApplication(VueApplication vueApplication) {
+        this.vueApplication = vueApplication;
+    }
+
+    /**
+     * Définit l'objet Carte avec celui spécifié.
+     *
+     * @param carte L'objet Carte à définir.
+     */
+    public void setCarte(Carte carte){this.carte=carte;}
+
+    /**
+     * Renvoie l'objet Carte.
+     *
+     * @return L'objet Carte.
+     */
+    public Carte getCarte(){return this.carte;}
 }
